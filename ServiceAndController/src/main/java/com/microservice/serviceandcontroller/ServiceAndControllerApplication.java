edcc5cb3f4e3e3420b094313a831a9d78259721f
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-08-03 11:44:41 +0430

    Add service module and add pom.xml dependencies

---
+++ ServiceAndController/src/main/java/com/microservice/serviceandcontroller/ServiceAndControllerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.serviceandcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAndControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAndControllerApplication.class, args);
+    }
+
+}
---
+++ ServiceAndController/src/test/java/com/microservice/serviceandcontroller/ServiceAndControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.serviceandcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAndControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
