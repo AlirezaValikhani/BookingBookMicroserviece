63a1b9afde31285738c88c4c00447abbd58d3739
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-08-03 10:15:27 +0430

    Add pom.xml dependencies and first files of project

---
+++ src/main/java/com/microservice/ReserveBook/ReserveBookApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.ReserveBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveBookApplication.class, args);
+	}
+
+}
