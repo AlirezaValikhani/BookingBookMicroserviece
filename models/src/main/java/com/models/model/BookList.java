e72d7de4ac3d37923bcb912a2d6747e4ec947df5
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-08-04 12:22:55 +0430

    Two pojo added for get list of object from other moduls

---
+++ models/src/main/java/com/models/model/BookList.java
@@ -0,0 +1,15 @@
+package com.models.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookList implements Serializable {
+    private List<Book> books;
+}
