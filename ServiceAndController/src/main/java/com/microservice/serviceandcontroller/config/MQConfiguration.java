a019508ae9ea28f3f83821738e94dc56a60c7a0b
Alireza Valikhani <alirezavalikhabi1998@gmail.com>
2022-08-04 12:17:21 +0430

    Rabbitmq configuration is complete and all queues are added

---
+++ ServiceAndController/src/main/java/com/microservice/serviceandcontroller/config/MQConfiguration.java
@@ -0,0 +1,46 @@
+package com.microservice.serviceandcontroller.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MQConfiguration {
+    public static final String QUEUE = "repository_queue";
+    public static final String EXCHANGE = "repository_exchange";
+    public static final String ROUTING_KEY = "Book_rk";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue,TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
--- repository/src/main/java/com/reservebook/repository/config/MQConfiguration.java
+++ repository/src/main/java/com/reservebook/repository/config/MQConfiguration.java
@@ -10,26 +10,77 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfiguration {
-    public static final String QUEUE = "repository_queue";
-    public static final String EXCHANGE = "repository_exchange";
-    public static final String ROUTING_KEY = "repository_routingKey";
+    public static final String  SAVE_BOOK_QUEUE = "save_book_queue";
+    public static final String FIND_BOOK_BY_ID_QUEUE = "find_book_by_id_queue";
+    public static final String UPDATE_BOOK_QUEUE = "update_book_queue";
+    public static final String DELETE_BOOK_QUEUE = "delete_book_queue";
+    public static final String FIND_ALL_BOOKS_QUEUE = "find_all_books_queue";
+    public static final String SEARCH_BOOKS_QUEUE = "search_books_queue";
+    public static final String  SAVE_USER_QUEUE = "save_user_queue";
+    public static final String FIND_USER_BY_ID_QUEUE = "find_user_by_id_queue";
+    public static final String UPDATE_USER_QUEUE = "update_user_queue";
+    public static final String DELETE_USER_QUEUE = "delete_user_queue";
+    public static final String FIND_ALL_USERS_QUEUE = "find_all_users_queue";
+    public static final String SEARCH_USERS_QUEUE = "search_users_queue";
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE);
+    public Queue saveBookQueue() {
+        return new Queue(SAVE_BOOK_QUEUE);
     }
 
     @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
+    public Queue findBookByIdQueue() {
+        return new Queue(FIND_BOOK_BY_ID_QUEUE);
     }
 
     @Bean
-    public Binding binding(Queue queue,TopicExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(ROUTING_KEY);
+    public Queue updateBookQueue() {
+        return new Queue(UPDATE_BOOK_QUEUE);
+    }
+
+    @Bean
+    public Queue deleteBookQueue() {
+        return new Queue(DELETE_BOOK_QUEUE);
+    }
+
+    @Bean
+    public Queue findAllBooksQueue() {
+        return new Queue(FIND_ALL_BOOKS_QUEUE);
+    }
+
+    @Bean
+    public Queue searchBooksQueue() {
+        return new Queue(SEARCH_BOOKS_QUEUE);
+    }
+
+    @Bean
+    public Queue saveUserQueue() {
+        return new Queue(SAVE_USER_QUEUE);
+    }
+
+    @Bean
+    public Queue findUserByIdQueue() {
+        return new Queue(FIND_USER_BY_ID_QUEUE);
+    }
+
+    @Bean
+    public Queue updateUserQueue() {
+        return new Queue(UPDATE_USER_QUEUE);
+    }
+
+    @Bean
+    public Queue deleteUserQueue() {
+        return new Queue(DELETE_USER_QUEUE);
+    }
+
+    @Bean
+    public Queue findAllUsersQueue() {
+        return new Queue(FIND_ALL_USERS_QUEUE);
+    }
+
+    @Bean
+    public Queue searchUsersQueue() {
+        return new Queue(SEARCH_USERS_QUEUE);
     }
 
     @Bean
